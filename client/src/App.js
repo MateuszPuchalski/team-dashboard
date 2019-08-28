0c3a297a00f633dedbc04b13d6e4ba14bb9a2474
MateuszPuchalski <puchalskimateusz1@gmail.com>
2019-08-28 22:21:39 +0200

    chane router directory

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,22 @@ import SeasonStats from "./components/seasonStats";
 import PhysicalForm from "./components/physicalForm";
 import Chart from "./components/chart";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import "./normalize.css";
 import "./main.css";
 
 export default class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="dashboard">
-          <Sidebar />
-          <Route path="/player/:id" component={PlayerInfo} />
-          <SeasonStats />
-          <PhysicalForm />
-          <Chart />
-        </div>
-      </Router>
+      <div className="dashboard">
+        <Sidebar />
+
+        <Route path="/player/:id" component={PlayerInfo} />
+
+        <SeasonStats />
+        <PhysicalForm />
+        <Chart />
+      </div>
     );
   }
 }
