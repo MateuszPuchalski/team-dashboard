8dfa983eba5e756b136244891dc1192f714f54b1
MateuszPuchalski <puchalskimateusz1@gmail.com>
2019-08-29 13:57:26 +0200

    delete arrow

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -11,18 +11,12 @@ class Sidebar extends Component {
     let avatars = [];
 
     for (let i = 1; i <= 6; i++) {
-      let className = `sidebar__item sidebar__item--${i + 1}`;
-      avatars.push(<Avatar id={i} className={className} />);
+      avatars.push(<Avatar id={i} />);
     }
     return (
       <div className="sidebar">
         <img src={process.env.PUBLIC_URL + `/herb.png`} id="herb" alt="herb" />
         {avatars}
-        <img
-          src={process.env.PUBLIC_URL + `/whitearrow.png`}
-          className="sidebar__item sidebar__item--8"
-          alt="herb"
-        />
       </div>
     );
   }
