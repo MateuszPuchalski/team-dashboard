1a466c1a14e3a1cb71231fe010167a0f200f8ca4
MateuszPuchalski <puchalskimateusz1@gmail.com>
2019-08-30 02:19:37 +0200

    add route for amount of goals player score in each game

--- api/routes/players.js
+++ api/routes/players.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var db = require("../db.js");
-
+// SELECT * FROM players JOIN (stats JOIN matches ON stats.match_id=matches.id) ON players.id=stats.player_id
 /* GET users listing. */
 router.get("/:playerId", function(req, res, next) {
   db.query(
@@ -13,4 +13,17 @@ router.get("/:playerId", function(req, res, next) {
   );
 });
 
+router.get("/:playerId/goals", function(req, res, next) {
+  db.query(
+    `SELECT * FROM stats WHERE player_id = ${req.params.playerId}`,
+    (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    }
+  );
+});
+
 module.exports = router;
+
+//SELECT player_id, against, name, surname, position, goals, height, weight, age, nr
+//FROM players JOIN (stats JOIN matches ON stats.match_id=matches.id) ON players.id=stats.player_id
