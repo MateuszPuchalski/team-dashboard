1ca93fd3e08bcfd1a8bf5f17598caac16891ffb5
MateuszPuchalski <puchalskimateusz1@gmail.com>
2019-09-03 01:54:19 +0200

    finaly only line left

--- client/src/components/chart.js
+++ client/src/components/chart.js
@@ -10,22 +10,8 @@ class Chart extends Component {
           {
             label: "Goals",
             data: [],
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)"
-            ],
-            borderColor: [
-              "rgba(255, 99, 132, 1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)"
-            ],
+            backgroundColor: [],
+            borderColor: [],
             borderWidth: 1
           }
         ]
@@ -77,25 +63,47 @@ class Chart extends Component {
               {
                 label: "Goals",
                 data: goals,
-                backgroundColor: [
-                  "rgba(255, 99, 132, 0.2)",
-                  "rgba(54, 162, 235, 0.2)",
-                  "rgba(255, 206, 86, 0.2)",
-                  "rgba(75, 192, 192, 0.2)",
-                  "rgba(153, 102, 255, 0.2)",
-                  "rgba(255, 159, 64, 0.2)"
-                ],
-                borderColor: [
-                  "rgba(255, 99, 132, 1)",
-                  "rgba(54, 162, 235, 1)",
-                  "rgba(255, 206, 86, 1)",
-                  "rgba(75, 192, 192, 1)",
-                  "rgba(153, 102, 255, 1)",
-                  "rgba(255, 159, 64, 1)"
-                ],
-                borderWidth: 1
+                backgroundColor: [],
+                borderColor: ["red"],
+                borderWidth: 5,
+                fill: false
               }
             ]
+          },
+          options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: { display: false },
+            tooltips: {
+              enabled: true,
+              backgroundColor: "red"
+            },
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    display: false,
+                    beginAtZero: true,
+                    stepSize: 2,
+                    suggestedMin: 0,
+                    suggestedMax: 10
+                  },
+                  gridLines: {
+                    display: false,
+                    drawBorder: false
+                  }
+                }
+              ],
+              xAxes: [
+                {
+                  ticks: { display: false },
+                  gridLines: {
+                    display: false,
+                    drawBorder: false
+                  }
+                }
+              ]
+            }
           },
           isLoading: false,
           urlPlayerId: this.props.match.params.id
