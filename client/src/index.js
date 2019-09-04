74f1a8b71204bb82969ebcf77f88396ef8ee484d
MateuszPuchalski <puchalskimateusz1@gmail.com>
2019-09-04 21:56:33 +0200

    ?

--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,10 @@ import "./index.css";
 import "./main.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 ReactDOM.render(
   <Router>
-    <App />
+    <Route component={App} />
   </Router>,
   document.getElementById("root")
 );
