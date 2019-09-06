073ce5135f4e44c0fa33285ab7a45a13b9c1cd45
MateuszPuchalski <puchalskimateusz1@gmail.com>
2019-09-06 14:41:19 +0200

    add some style to line

--- client/src/components/chart.js
+++ client/src/components/chart.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
+import "chart.js";
 import { Line } from "react-chartjs-2";
-import ChartDataLabels from "chartjs-plugin-datalabels";
+import "chartjs-plugin-datalabels";
+import "chartjs-plugin-style";
 
 export default function Chart(props) {
   const [goals, setGoals] = useState([]);
@@ -14,24 +16,43 @@ export default function Chart(props) {
       labels: clubNames,
       datasets: [
         {
+          backgroundColor: "blue",
           label: "Goals",
           data: goals,
           borderColor: "red",
           borderWidth: 5,
-          fill: false
+          fill: false,
+          outerGlowWidth: 25,
+          outerGlowColor: "red",
+          pointRadius: 0,
+          shadowOffsetX: 3,
+          shadowOffsetY: 3,
+          shadowBlur: 20,
+          shadowColor: "rgba(0, 0, 0, 0.5)"
         }
       ]
     },
     options: {
       responsive: true,
       maintainAspectRatio: false,
+      tooltips: { enabled: false },
+      layout: {
+        padding: {
+          left: 50,
+          right: 50,
+          top: 30,
+          bottom: 20
+        }
+      },
       plugins: {
         // Change options for ALL labels of THIS CHART
         datalabels: {
+          display: true,
           color: "white",
           align: "top",
           borderWidth: 2,
           borderRadius: 3,
+          font: { size: 17 },
           backgroundColor: context => {
             let index = context.dataIndex;
             let value = context.dataset.data[index];
@@ -46,7 +67,7 @@ export default function Chart(props) {
         yAxes: [
           {
             ticks: {
-              display: true,
+              display: false,
               beginAtZero: true,
               stepSize: 2,
               suggestedMin: 0,
@@ -54,7 +75,7 @@ export default function Chart(props) {
             },
             gridLines: {
               display: false,
-              drawBorder: true
+              drawBorder: false
             }
           }
         ],
@@ -82,16 +103,16 @@ export default function Chart(props) {
       canvas.width / 2,
       0
     );
-    console.log(colors);
-    gradient.addColorStop(0, "red");
-    gradient.addColorStop(0.3, "orange");
-    gradient.addColorStop(0.65, "green");
+    gradient.addColorStop(0.15, "#E43025");
+    gradient.addColorStop(0.3, "#DF7604");
+    gradient.addColorStop(0.65, "#01C23C");
     return gradient;
   };
 
   const getChartData = canvas => {
     const data = chartData.data;
     data.datasets[0]["borderColor"] = setGradientColor(canvas);
+    data.datasets[0]["outerGlowColor"] = setGradientColor(canvas);
     return data;
   };
 
