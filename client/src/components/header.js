1e448625ba1ca6d9d736ce033f88e0d6876702da
MateuszPuchalski <puchalskimateusz1@gmail.com>
2019-09-06 02:56:10 +0200

    add header component

---
+++ client/src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div>OVERVIEW</div>
+      <div>TRAINING</div>
+      <div>STATS</div>
+      <div>TRANSFERS</div>
+      <div>REPORTS</div>
+    </div>
+  );
+}
