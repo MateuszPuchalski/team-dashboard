fecdfc19a15dc4a57d27fce32033eebf8e8dbe46
MateuszPuchalski <puchalskimateusz1@gmail.com>
2019-09-11 20:55:12 +0200

    refactor using hooks and add scrolling

--- client/src/components/avatar.js
+++ client/src/components/avatar.js
@@ -1,44 +1,37 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "../main.css";
 import { Link } from "react-router-dom";
-class Avatar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      surname: ""
-    };
-  }
 
-  componentDidMount() {
-    fetch(`/players/${this.props.id}`)
-      .then(res => res.json())
-      .then(result => this.setState(result[0]));
-  }
+export default function Avatar({ id }) {
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
 
-  render() {
-    // console.log(window.location.href[window.location.href.length - 1]);
-    // let classNames = "sidebar__item";
-    // console.log(this.state.id);
-    // if (
-    //   window.location.href[window.location.href.length - 1] == this.state.id
-    // ) {
-    //   classNames += " avatarSelected";
-    // }
-    return (
-      <div className={"sidebar__item"}>
-        <Link to={`/players/${this.props.id}`}>
-          <img
-            src={
-              process.env.PUBLIC_URL +
-              `/avatars/${this.state.name}${this.state.surname}.png`
-            }
-            alt={`${this.state.name} ${this.state.surname}`}
-          />
-        </Link>
-      </div>
-    );
-  }
+  const fetchData = async id => {
+    const data = await fetch(`/players/${id}`);
+    const items = await data.json();
+    setName(items[0].name);
+    setSurname(items[0].surname);
+  };
+  useEffect(() => {
+    fetchData(id);
+  });
+
+  // console.log(window.location.href[window.location.href.length - 1]);
+  // let classNames = "sidebar__item";
+  // console.log(this.state.id);
+  // if (
+  //   window.location.href[window.location.href.length - 1] == this.state.id
+  // ) {
+  //   classNames += " avatarSelected";
+  // }
+  return (
+    <div className={"sidebar__item"}>
+      <Link to={`/players/${id}`}>
+        <img
+          src={process.env.PUBLIC_URL + `/avatars/${name}${surname}.png`}
+          alt={`${name} ${surname}`}
+        />
+      </Link>
+    </div>
+  );
 }
-
-export default Avatar;
