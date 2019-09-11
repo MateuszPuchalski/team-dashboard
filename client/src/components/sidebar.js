17b8bf58ed19f6b780fe5de92c0da15554ff2a94
MateuszPuchalski <puchalskimateusz1@gmail.com>
2019-09-11 20:57:20 +0200

    refactor using hooks and add scrolling

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -1,25 +1,37 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Avatar from "./avatar";
 import "../main.css";
 
-class Sidebar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    let avatars = [];
+export default function Sidebar(props) {
+  const [id, setId] = useState(1);
+  // const [avatars, setAvatars] = useState([]);
+  // console.log(id);
+  // console.log(avatars);
 
-    for (let i = 1; i <= 6; i++) {
-      avatars.push(<Avatar id={i} />);
-    }
-    return (
-      <div className="sidebar">
-        <img src={process.env.PUBLIC_URL + `/herb.png`} id="herb" alt="herb" />
-        {avatars}
-      </div>
-    );
+  function renderAvatar(id) {
+    return <Avatar id={id} />;
   }
+
+  function oneUp() {
+    setId(id + 1);
+    console.log(id);
+  }
+
+  return (
+    <div className="sidebar">
+      <img src={process.env.PUBLIC_URL + `/herb.png`} id="herb" alt="herb" />
+      {renderAvatar(id)}
+      {renderAvatar(id + 1)}
+      {renderAvatar(id + 2)}
+      {renderAvatar(id + 3)}
+      {renderAvatar(id + 4)}
+      {renderAvatar(id + 5)}
+      <img
+        onClick={oneUp}
+        src={process.env.PUBLIC_URL + `/arrow.png`}
+        id="arrow"
+        alt="arrow"
+      />
+    </div>
+  );
 }
-
-export default Sidebar;
