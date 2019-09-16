24dda03b04d1a01f1153f26ca504777a5063834f
MateuszPuchalski <puchalskimateusz1@gmail.com>
2019-09-16 23:16:03 +0200

    add more avatars

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -4,34 +4,38 @@ import "../main.css";
 
 export default function Sidebar(props) {
   const [id, setId] = useState(1);
-  // const [avatars, setAvatars] = useState([]);
-  // console.log(id);
-  // console.log(avatars);
 
   function renderAvatar(id) {
     return <Avatar id={id} />;
   }
 
-  function oneUp() {
-    setId(id + 1);
-    console.log(id);
-  }
-
   return (
     <div className="sidebar">
       <img src={process.env.PUBLIC_URL + `/herb.png`} id="herb" alt="herb" />
-      {renderAvatar(id)}
-      {renderAvatar(id + 1)}
-      {renderAvatar(id + 2)}
-      {renderAvatar(id + 3)}
-      {renderAvatar(id + 4)}
-      {renderAvatar(id + 5)}
-      <img
-        onClick={oneUp}
-        src={process.env.PUBLIC_URL + `/arrow.png`}
-        id="arrow"
-        alt="arrow"
-      />
+      <div className="avatars">
+        {renderAvatar(id)}
+        {renderAvatar(id + 1)}
+        {renderAvatar(id + 2)}
+        {renderAvatar(id + 3)}
+        {renderAvatar(id + 4)}
+        {renderAvatar(id + 5)}
+        {renderAvatar(id + 6)}
+        {renderAvatar(id + 7)}
+        {renderAvatar(id + 8)}
+        {renderAvatar(id + 9)}
+        {renderAvatar(id + 10)}
+        {renderAvatar(id + 11)}
+        {renderAvatar(id + 12)}
+        {renderAvatar(id + 13)}
+        {renderAvatar(id + 14)}
+        {renderAvatar(id + 15)}
+        {renderAvatar(id + 16)}
+        {renderAvatar(id + 17)}
+        {renderAvatar(id + 18)}
+        {renderAvatar(id + 19)}
+      </div>
+
+      <img src={process.env.PUBLIC_URL + `/arrow.png`} id="arrow" alt="arrow" />
     </div>
   );
 }
