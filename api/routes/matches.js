6d85635bb79dded9c5d6f01bd20e69f110457b36
MateuszPuchalski <puchalskimateusz1@gmail.com>
2019-09-28 15:15:51 +0200

    add new get option

--- api/routes/matches.js
+++ api/routes/matches.js
@@ -20,4 +20,16 @@ router.get("/:matchId", function(req, res, next) {
   );
 });
 
+router.get("/logos/:id", function(req, res, next) {
+  db.query(
+    `SELECT matches.against, matches.date, stats.player_id, stats.match_id FROM stats LEFT JOIN matches ON matches.id = stats.match_id JOIN players ON players.id = stats.player_id WHERE player_id = ${req.params.id}`,
+    (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    }
+  );
+});
+
+// SELECT matches.against, matches.date, stats.player_id, stats.match_id FROM stats LEFT JOIN matches ON matches.id = stats.match_id JOIN players ON players.id = stats.player_id WHERE player_id = 1
+
 module.exports = router;
