dcb98922e60e8bde40abcd3e2715dca5bb41619c
MateuszPuchalski <puchalskimateusz1@gmail.com>
2019-09-28 15:14:40 +0200

    change physcialForm to func component

--- client/src/components/physicalForm.js
+++ client/src/components/physicalForm.js
@@ -1,56 +1,43 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Logo from "./logo";
 import GoalsPerMatch from "./goalsPerMatch";
-class PhysicalForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-      matches: [],
-      error: null
-    };
-  }
 
-  fetchMatches() {
-    fetch("/matches")
-      .then(response => response.json())
-      .then(data => {
-        data.sort((a, b) => new Date(a.date) - new Date(b.date));
-        this.setState({
-          matches: data.slice(-5),
-          isLoading: false
-        });
-      });
-  }
-  componentDidMount() {
-    this.fetchMatches();
-  }
+export default function PhysicalForm(props) {
+  const [matches, setMatches] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  render() {
-    const { isLoading, matches, error } = this.state;
-    return (
-      <>
-        <GoalsPerMatch id={this.props.match.params.id} />
+  const fetchMatches = async () => {
+    setLoading(true);
+    const data = await fetch(`/matches/logos/${props.match.params.id}`);
+    const items = await data.json();
+    items.sort((a, b) => new Date(a.date) - new Date(b.date));
+    setMatches(items.slice(-5));
+    setLoading(false);
+  };
 
-        <div className="physicalForm">
-          {error ? <p>{error.message}</p> : null}
+  useEffect(() => {
+    fetchMatches();
+    console.log(matches);
+  }, [props.match.params.id]);
 
-          {!isLoading ? (
-            <>
-              <Logo match={matches[0]} />
-              <Logo match={matches[1]} />
-              <Logo match={matches[2]} />
-              <Logo match={matches[3]} />
-              <Logo match={matches[4]} />
-              {/* <Logo match={matches[5]} /> */}
-            </>
-          ) : (
-            <h3>Loading...</h3>
-          )}
-        </div>
-      </>
-    );
-  }
+  return (
+    <>
+      <GoalsPerMatch id={props.match.params.id} />
+      <div className="physicalForm">
+        {!loading && matches.length > 4 ? (
+          <>
+            <Logo match={matches[0]} />
+            <Logo match={matches[1]} />
+            <Logo match={matches[2]} />
+            <Logo match={matches[3]} />
+            <Logo match={matches[4]} />
+          </>
+        ) : (
+          <h3>LOADING...</h3>
+        )}
+
+        {/* <Logo match={matches[5]} /> */}
+      </div>
+    </>
+  );
 }
-
-export default PhysicalForm;
