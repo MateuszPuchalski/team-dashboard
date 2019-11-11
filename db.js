a4b3667a710807de91696fb30ae12c0fe94a550a
MateuszPuchalski <puchalskimateusz1@gmail.com>
2019-11-12 00:55:34 +0100

    .

--- db.js
+++ db.js
@@ -1,12 +1,19 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
+  host: "jfrpocyduwfg38kq.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+  user: "w823akrx2xywm1k9",
+  password: "vquxedcebm1q41ua",
   charset: "utf8",
-  database: "dashboard"
+  database: "pqrr08fhcy6z1zhv"
 });
 
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   charset: "utf8",
+//   database: "dashboard"
+// });
 // Connect
 db.connect(err => {
   if (err) {
