a6b76b2aa32fc1c7e117e6bd3b97d6b512434cd1
MateuszPuchalski <puchalskimateusz1@gmail.com>
2019-11-12 18:54:44 +0100

    .

--- app.js
+++ app.js
@@ -45,5 +45,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
-
+const port = process.env.PORT || 3001;
+app.listen(port);
 module.exports = app;
