6c8dcac7245b6262a1b7ce93fa8d374a97c66c9e
MateuszPuchalski <puchalskimateusz1@gmail.com>
2019-11-12 16:08:35 +0100

    .

--- db.js
+++ db.js
@@ -1,19 +1,24 @@
 const mysql = require("mysql");
 
-const db = mysql.createConnection({
-  host: "jfrpocyduwfg38kq.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
-  user: "w823akrx2xywm1k9",
-  password: "vquxedcebm1q41ua",
-  charset: "utf8",
-  database: "pqrr08fhcy6z1zhv"
-});
-// database: "pqrr08fhcy6z1zhv"
 // const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
+//   host: "jfrpocyduwfg38kq.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+//   user: "w823akrx2xywm1k9",
+//   password: "vquxedcebm1q41ua",
 //   charset: "utf8",
-//   database: "dashboard"
+//   database: "pqrr08fhcy6z1zhv"
 // });
+// database: "pqrr08fhcy6z1zhv"
+if (process.env.JAWSDB_URL) {
+  const db = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    charset: "utf8",
+    database: "dashboard"
+  });
+}
+
 // Connect
 db.connect(err => {
   if (err) {
