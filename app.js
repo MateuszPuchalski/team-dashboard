5350b0df8620701ee712a551aaa47ad6f9cca2dc
MateuszPuchalski <puchalskimateusz1@gmail.com>
2019-11-12 01:39:12 +0100

    .

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ app.use(cookieParser());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "client", "build", "public", "index.html")
+    );
   });
 } else {
   app.use(express.static(path.join(__dirname, "public")));
