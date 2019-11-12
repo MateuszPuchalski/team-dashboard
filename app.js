d60be2006249d16ab9731b9b46820ad71acd4810
MateuszPuchalski <puchalskimateusz1@gmail.com>
2019-11-12 19:42:30 +0100

    .

--- app.js
+++ app.js
@@ -13,24 +13,24 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
-
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use("/", indexRouter);
+app.use("/players", playersRouter);
+app.use("/matches", matchesRouter);
+
 if (process.env.NODE_ENV === "production") {
-  app.use(path.join(__dirname, "client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 } else {
   app.use(express.static(path.join(__dirname, "public")));
 }
-
-app.use("/", indexRouter);
-app.use("/players", playersRouter);
-app.use("/matches", matchesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
