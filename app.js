402891fff73854ce45ad38ba9d608212b2eb4254
MateuszPuchalski <puchalskimateusz1@gmail.com>
2019-11-13 01:15:18 +0100

    .

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(express.static(__dirname));
 app.use("/", indexRouter);
 app.use("/players", playersRouter);
 app.use("/matches", matchesRouter);
