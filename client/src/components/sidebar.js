7aeda7b5f39acabeadd3ef280580b030cc78b2d5
MateuszPuchalski <puchalskimateusz1@gmail.com>
2019-11-19 02:50:25 +0100

    refactor sidbar

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -5,37 +5,36 @@ import { Link } from "react-router-dom";
 
 export default function Sidebar(props) {
   const [id, setId] = useState(1);
+  const [players, setPlayers] = useState([]);
+  const getAllPlayers = async () => {
+    const data = await fetch("/players");
+    const items = await data.json();
+    console.log(items);
+    setPlayers(items);
+  };
 
-  function renderAvatar(id) {
-    return (
-      <Link to={`/players/${id}`}>
-        <Avatar id={id} />
-      </Link>
-    );
-  }
+  const renderAvatar = players => {
+    const linkedPlayers = players.map(player => {
+      return (
+        <Link to={`/players/${player.id}`}>
+          <Avatar id={player.id} />
+        </Link>
+      );
+    });
+
+    return linkedPlayers;
+  };
+
+  useEffect(() => {
+    getAllPlayers();
+    console.log(players);
+  }, []);
 
   return (
     <div className="sidebar">
       <img src={process.env.PUBLIC_URL + `/herb.webp`} id="herb" alt="herb" />
       <div className="avatars">
-        {renderAvatar(id)}
-        {renderAvatar(id + 1)}
-        {renderAvatar(id + 2)}
-        {renderAvatar(id + 3)}
-
-        {renderAvatar(id + 5)}
-        {renderAvatar(id + 6)}
-        {renderAvatar(id + 7)}
-        {renderAvatar(id + 8)}
-        {renderAvatar(id + 9)}
-        {renderAvatar(id + 10)}
-        {renderAvatar(id + 11)}
-        {renderAvatar(id + 12)}
-        {renderAvatar(id + 13)}
-        {renderAvatar(id + 14)}
-        {renderAvatar(id + 15)}
-        {renderAvatar(id + 16)}
-        {renderAvatar(id + 17)}
+        {players[0] ? renderAvatar(players) : <h3>LOADING...</h3>}
       </div>
 
       <img src={process.env.PUBLIC_URL + `/arrow.png`} id="arrow" alt="arrow" />
