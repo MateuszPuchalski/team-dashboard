6f77ae1a1318ea131df35657e7251bb926dd1635
MateuszPuchalski <puchalskimateusz1@gmail.com>
2019-11-19 02:50:13 +0100

    fix player route

--- routes/players.js
+++ routes/players.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var db = require("../db.js");
 // SELECT * FROM players JOIN (stats JOIN matches ON stats.match_id=matches.id) ON players.id=stats.player_id
 /* GET users listing. */
-router.get("/:playerId", function(req, res, next) {
+router.get("/", function(req, res, next) {
   db.query(`SELECT * FROM players`, (err, result) => {
     if (err) throw err;
     res.send(result);
