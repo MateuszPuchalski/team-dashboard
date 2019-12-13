c183c87d6172fcb51a5555d7c16542334e5176e0
MateuszPuchalski <puchalskimateusz1@gmail.com>
2019-12-13 01:13:19 +0100

    link to stats from header

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 export default function Header() {
+  // let { id } = useParams();
   return (
     <div className="header">
       <div>OVERVIEW</div>
 
       <div>TRAINING</div>
+      <div>
+        <Link to={`/stats/1`}>STATS</Link>
+      </div>
 
-      <div>STATS</div>
       <div>TRANSFERS</div>
       <div>REPORTS</div>
     </div>
