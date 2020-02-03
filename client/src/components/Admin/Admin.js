869c162b0594c7a1a5440b7d340e0cd616d33f35
MateuszPuchalski <puchalskimateusz1@gmail.com>
2020-02-03 03:00:59 +0100

    add admin matches

--- client/src/components/Admin/Admin.js
+++ client/src/components/Admin/Admin.js
@@ -4,6 +4,8 @@ import styled from "styled-components";
 import AdminAddClub from "./AdminAddClub";
 import AdminAddPlayer from "./AdminAddPlayer";
 import AdminShowPlayers from "./AdminShowPlayers";
+import AdminMatches from "./Matches/AdminMatches";
+import AdminShowMatches from "./Matches/AdminSchowMatches";
 
 const Wrapper = styled.div`
   display: flex;
@@ -11,10 +13,14 @@ const Wrapper = styled.div`
 `;
 export default function Admin() {
   return (
-    <Wrapper>
-      <AdminAddClub />
-      <AdminAddPlayer />
-      <AdminShowPlayers />
-    </Wrapper>
+    <>
+      <Wrapper>
+        <AdminAddClub />
+        <AdminAddPlayer />
+        <AdminShowPlayers />
+        <AdminMatches />
+      </Wrapper>
+      <AdminShowMatches />
+    </>
   );
 }
