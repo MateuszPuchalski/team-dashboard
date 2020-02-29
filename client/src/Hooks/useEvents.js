fb93b3f1f3c0937d92cc1fc0bce3d61e91d8c91f
MateuszPuchalski <puchalskimateusz1@gmail.com>
2020-02-29 22:27:52 +0100

    s

--- client/src/Hooks/useEvents.js
+++ client/src/Hooks/useEvents.js
@@ -3,9 +3,19 @@ import React, { useState, useEffect } from "react";
 export default function useEvents({ matchId = null, playerId = null }) {
   const [events, setEvents] = useState();
   const [loading, setLoading] = useState();
-  console.log({ matchId: matchId, playerId: playerId });
+  console.log();
   useEffect(() => {
     setLoading(true);
+
+    if (!matchId && !playerId) {
+      fetch(`/api/events`)
+        .then(res => res.json())
+        .then(data => {
+          setEvents(data);
+          setLoading(false);
+        });
+      return;
+    }
 
     if (matchId && playerId) {
       fetch(`/api/events/player/${playerId}/match/${matchId}/`)
