413213758a85551050121d78b22bf6dd1607c5cb
unknown <puchalskimateusz1@gmail.com>
2020-04-29 01:55:27 +0200

    add some animations

--- client/src/components/EventPicker/PlayerButton.js
+++ client/src/components/EventPicker/PlayerButton.js
@@ -1,25 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styled from "styled-components";
-
-import { eventActions } from "../../_actions";
+import { useSpring, animated } from "react-spring";
+import { eventAddingActions } from "../../_actions";
 import { useDispatch } from "react-redux";
 
-const Wrapper = styled.div`
+const Wrapper = styled(animated.div)`
   background: ${(props) => props.theme.primary};
-  width: 120px;
-  height: 120px;
+  width: 80%;
   margin: 10px;
   border-radius: 5px;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  flex-direction: row;
+  justify-content: space-evenly;
   align-items: center;
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
-export default function PlayerButton({ name, nr }) {
+export default function PlayerButton({ name, nr, active, setActive }) {
   const dispatch = useDispatch();
+  const ref = useRef();
+  const props = useSpring({
+    background:
+      ref == active
+        ? "linear-gradient(270deg, rgba(0,180,255,1) 0%, rgba(0,120,255,1) 100% "
+        : "linear-gradient(270deg,rgba(255, 255, 255, 1) 0%,rgba(255, 255, 255, 1) 100%)",
+  });
   return (
-    <Wrapper onClick={() => dispatch(eventActions.setPlayer(name))}>
+    <Wrapper
+      ref={ref}
+      style={props}
+      onClick={() => {
+        setActive(ref);
+        dispatch(eventAddingActions.setPlayer(name));
+      }}
+    >
+      <svg width="60" height="60">
+        <circle
+          cx="30"
+          cy="30"
+          r="25"
+          stroke="black"
+          stroke-width="2"
+          fill="rgba(0,0,0,.1)"
+        />
+      </svg>
       <h3>{name}</h3>
       <h3>{nr}</h3>
     </Wrapper>
