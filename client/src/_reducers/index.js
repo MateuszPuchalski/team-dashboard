569c748249592c3e9a44c06b58ec09280f215556
unknown <puchalskimateusz1@gmail.com>
2020-04-29 01:53:26 +0200

    remove test reducers

--- client/src/_reducers/index.js
+++ client/src/_reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from "redux";
 
-import { test } from "./test.reducer";
 import { authentication } from "./authentication.reducer";
 import { registration } from "./registration.reducer";
 import { eventShape } from "./eventShape.reducer";
 
 const rootReducer = combineReducers({
-  test,
   authentication,
   registration,
   eventShape,
