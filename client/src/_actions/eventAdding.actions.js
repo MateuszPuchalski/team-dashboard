a0027956adc10d34deed85ffedcbaac1dc049fc6
unknown <puchalskimateusz1@gmail.com>
2020-04-29 23:59:17 +0200

    add actions

--- client/src/_actions/eventAdding.actions.js
+++ client/src/_actions/eventAdding.actions.js
@@ -3,6 +3,8 @@ export const eventAddingActions = {
   setPlayer,
   setEvent,
   setEventType,
+  getMatch,
+  getPlayers,
 };
 
 function setPlayer(player) {
@@ -13,4 +15,33 @@ function setEvent(event) {
 }
 function setEventType(eventType) {
   return { type: eventAddingConstants.SET_EVENT_TYPE, eventType };
+}
+function getMatch(matchId) {
+  return (dispatch) => {
+    fetch(`/api/matches/${matchId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: eventAddingConstants.GET_MATCH, match: data });
+      });
+  };
+}
+function getPlayers(side, clubId) {
+  return (dispatch) => {
+    fetch(`/api/players/club/${clubId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (side == "home") {
+          dispatch({
+            type: eventAddingConstants.GET_HOME_PLAYERS,
+            players: data,
+          });
+        }
+        if (side == "away") {
+          dispatch({
+            type: eventAddingConstants.GET_AWAY_PLAYERS,
+            players: data,
+          });
+        }
+      });
+  };
 }
