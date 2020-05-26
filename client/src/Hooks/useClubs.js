37dd0d78ce373121767f6bbec36cd9abd74004f7
unknown <puchalskimateusz1@gmail.com>
2020-05-26 18:50:16 +0200

    refactor

--- client/src/Hooks/useClubs.js
+++ client/src/Hooks/useClubs.js
@@ -1,27 +1,22 @@
 import React, { useState, useEffect } from "react";
 
-export default function useClubs({ clubId }) {
-  const [clubs, setClubs] = useState();
-  const [loading, setLoading] = useState();
+export default function useClubs() {
+  const [clubs, setClubs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(true);
-    if (clubId) {
-      fetch(`/api/clubs/${clubId}`)
-        .then(res => res.json())
-        .then(data => {
-          setClubs(data);
-          setLoading(false);
-        });
-      return;
-    } else {
-      fetch(`/api/clubs`)
-        .then(res => res.json())
-        .then(data => {
-          setClubs(data);
-          setLoading(false);
-        });
-    }
+    const fetchClubs = async () => {
+      setLoading(true);
+      try {
+        const data = await (await fetch(`/api/clubs`)).json();
+        console.log(data);
+        setClubs(data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchClubs();
   }, []);
 
   return [loading, clubs];
