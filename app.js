408fe79ac8a099404663afa98a62cbe5fcd3e8d9
unknown <puchalskimateusz1@gmail.com>
2020-06-12 22:53:06 +0200

    add apollo and graphql

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const cloudinary = require("cloudinary").v2;
 const path = require("path");
-const cors = require("cors");
+
+const PlayerModel = require("./models/player.model");
+const ClubModel = require("./models/club.model");
+const EventModel = require("./models/event.model");
+const MatchModel = require("./models/match.model");
+// const newEvent = new EventModel({
+//   matchId: "5e3758f1e60e452598df6397",
+//   type: "Throw",
+//   timestamp: 123,
+//   throw: {
+//     endLocation: [1, 1],
+//     outcome: "Goal",
+//     technique: "Hip Shot",
+//   },
+//   location: [1, 1],
+//   player: "5e3606a51dba6b0ac451eb42",
+// });
+// newEvent.save().then((event) => console.log(event));
+
+const { ApolloServer, gql } = require("apollo-server");
 
 require("dotenv").config();
 
 const mongoose = require("mongoose");
 
 const uri = process.env.ATLAS_URI;
+
 const CLOUDINARY_NAME = process.env.CLOUDINARY_NAME;
 const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
 const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
@@ -28,32 +48,150 @@ const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB Connected");
 });
-// const auth = require("./middleware/auth");
-const playersRouter = require("./routes/api/players");
-const competitionsRouter = require("./routes/api/competitions");
-const clubsRouter = require("./routes/api/clubs");
-const matchesRouter = require("./routes/api/matches");
-const usersRouter = require("./routes/api/users");
-const authRouter = require("./routes/api/auth");
-const testRouter = require("./routes/api/test");
-const eventRouter = require("./routes/api/events");
 
 const app = express();
 app.disable("x-powered-by");
-app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/test", testRouter);
-app.use("/api/players", playersRouter);
-app.use("/api/competitions", competitionsRouter);
-app.use("/api/clubs", clubsRouter);
-app.use("/api/matches", matchesRouter);
-app.use("/api/users", usersRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/events", eventRouter);
-console.log({ NODE_ENV: process.env.NODE_ENV });
+const typeDefs = gql`
+  type Query {
+    hello: String
+    playerOne: Player
+    playerById(id: String!): Player
+    playerByClub(clubId: String!): [Player]!
+    playerMany: [Player]!
+    clubMany: [Club]!
+    eventMany: [Event]!
+    eventByMatch(matchId: String!): [Event]!
+    eventByPlayer(playerId: String!): [Event]!
+    eventThrowByPlayer(playerId: String!): [Event]!
+    matchMany: [Match]!
+    matchById(matchId: String!): Match
+  }
+  type Player {
+    id: ID!
+    name: String!
+    currentClub: Club
+    position: String
+    weight: String
+    height: String
+    jerseyNumber: Int
+    avatar: String
+  }
+  type Match {
+    id: ID!
+    date: String
+    homeTeam: Club!
+    awayTeam: Club!
+    homeScore: Int
+    awayScore: Int
+    ytId: String
+  }
+  type Club {
+    id: ID!
+    name: String
+    logo: String
+  }
+  interface Event {
+    id: ID!
+    matchId: String
+    type: String
+    timestamp: Float
+  }
+  type SomeEvent implements Event {
+    id: ID!
+    matchId: String!
+    type: String
+    timestamp: Float!
+  }
+  type ThrowEvent implements Event {
+    id: ID!
+    matchId: String
+    team: String
+    type: String
+    timestamp: Float
+    player: Player
+    location: [Float]
+    throw: Throw
+  }
+  type Throw {
+    endLocation: [Float]
+    outcome: String
+    technique: String
+  }
+  type TurnoverEvent implements Event {
+    id: ID!
+    matchId: String
+    team: String
+    type: String
+    timestamp: Float
+    player: Player
+    location: [Float]
+    turnover: Turnover
+  }
+  type Turnover {
+    type: String
+  }
+  type PunishmentEvent implements Event {
+    id: ID!
+    matchId: String
+    team: String
+    type: String
+    timestamp: Float
+    player: Player
+    location: [Float]
+    punishment: Punishment
+  }
+  type Punishment {
+    type: String
+  }
+`;
+
+const resolvers = {
+  Event: {
+    __resolveType(event, context, info) {
+      if (event.throw) {
+        return "ThrowEvent";
+      }
+      if (event.turnover) {
+        return "TurnoverEvent";
+      }
+      if (event.punishment) {
+        return "PunishmentEvent";
+      }
+      return "SomeEvent";
+    },
+  },
+  Query: {
+    playerOne: () => PlayerModel.findOne().populate(["currentClub"]),
+    playerMany: () => PlayerModel.find().populate(["currentClub"]),
+    playerById: (_, args) =>
+      PlayerModel.findById(args.id).populate(["currentClub"]),
+    playerByClub: (_, args) =>
+      PlayerModel.find({ currentClub: args.clubId }).populate(["currentClub"]),
+    clubMany: () => ClubModel.find(),
+    eventMany: () => EventModel.find(),
+    eventByMatch: (_, args) =>
+      EventModel.find({
+        matchId: mongoose.Types.ObjectId(args.matchId),
+      }).populate(["player"]),
+    eventByPlayer: (_, args) =>
+      EventModel.find({
+        player: mongoose.Types.ObjectId(args.playerId),
+      }),
+    eventThrowByPlayer: (_, args) =>
+      EventModel.find({
+        player: mongoose.Types.ObjectId(args.playerId),
+        type: "Throw",
+      }),
+    matchMany: () => MatchModel.find().populate(["homeTeam", "awayTeam"]),
+    matchById: (_, args) =>
+      MatchModel.findById(args.matchId).populate(["homeTeam", "awayTeam"]),
+  },
+};
+
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
@@ -64,6 +202,8 @@ if (process.env.NODE_ENV === "production") {
 }
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Listening on port: ${port}`);
-});
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server
+  .listen({ port })
+  .then(({ url }) => console.log(`Server running at: ${url}`));
