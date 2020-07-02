f7319e05a01dd053d0854c0ae747003917906499
mat <puchalskimateusz1@gmail.com>
2020-07-02 12:52:23 +0200

    remove

--- middleware/isAuthenticated.js
+++
@@ -1,9 +0,0 @@
-function isAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    next();
-  } else {
-    res.status(401).json({ error: "User not authenticated" });
-  }
-}
-
-module.exports = isAuthenticated;
