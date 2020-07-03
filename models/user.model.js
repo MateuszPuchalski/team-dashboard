db3bbcb6553e99625eb6835b36fa50fbd17fd2d9
mat <puchalskimateusz1@gmail.com>
2020-07-03 21:00:01 +0200

    array of roles

--- models/user.model.js
+++ models/user.model.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
   {
-    role: { type: String, required: true }, // array of string for multiple roles
+    role: { type: [String], required: true }, // array of string for multiple roles
     password: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
   },
   { timestamps: true }
 );
