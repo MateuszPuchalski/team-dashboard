a09ac1a8f6ce7c6343f5526e324315f2a529de86
mat <puchalskimateusz1@gmail.com>
2020-07-27 18:51:17 +0200

    add Stats

---
+++ client/src/Stats/Stats.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import useMatchStats from "./useMatchStats";
+export default function Stats({ matchId }) {
+  const [loading, error, matchInfo] = useMatchStats(matchId); //"5e6553c81c9d44000046425e"
+  if (error) return <h3>SOMETHING WENT WRONG</h3>;
+  if (loading) return <h3>LOADING...</h3>;
+  if (matchInfo) {
+    console.log({ matchInfo: matchInfo });
+    return (
+      <div>
+        <h3>
+          HOME TEAM: {matchInfo.homeTeam.name} Efficiency:{" "}
+          {matchInfo.homeTeam.throws.efficiency}
+        </h3>
+        <h3>
+          AWAY TEAM: {matchInfo.awayTeam.name} Efficiency:{" "}
+          {matchInfo.awayTeam.throws.efficiency}
+        </h3>
+        <div style={{ background: "red" }}>
+          <h3>TOTAL THROWS: {matchInfo.throws.total}</h3>
+          <h3>MISS THROWS: {matchInfo.throws.miss}</h3>
+          <h3>GOALS: {matchInfo.throws.goals}</h3>
+          <h3>MATCH THROW EFFICIENCY: {matchInfo.throws.efficiency}</h3>;
+        </div>
+      </div>
+    );
+  }
+}
